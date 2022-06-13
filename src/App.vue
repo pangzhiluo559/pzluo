b342312da87d263cf371401808eab7a6995643c3
luozi <13060098289@163.com>
2022-06-13 17:47:41 +0800

    自动部署

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import Name from './components/name.vue';
 </script>
 
 <template>
-  <img class=" mx-auto" alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <Name name="pzluo"></Name>
 </template>
 
 <style>
@@ -14,8 +13,10 @@ import HelloWorld from './components/HelloWorld.vue'
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+body {
+  margin: 0;
+  padding: 0;
+  background: #000000;
 }
 </style>
---
+++ src/components/name.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+defineProps({
+  name: String
+})
+// const textColor = ['text-green-500', 'text-red-500', 'text-blue-500', 'text-yellow-500']
+const activeClass = 'active'
+
+const textColor = ref(['text-green-600', 'text-red-500', 'text-blue-800', 'text-yellow-500','text-purple-500'])
+
+const index = ref(0)
+
+setInterval(() => {
+  index.value = (index.value + 1) % 5
+}, 1000)
+
+</script>
+
+
+<template>
+    <div class="relative w-32 mx-auto">
+        <div class="relative text-center text-4xl font-bold" :class="textColor[index]">{{name}}</div>
+        <div class="absolute top-4 left-4 w-10 h-0.5 bg-white"></div>
+        <div class="absolute top-5 left-20 w-12 h-0.5 bg-white"></div>
+        <div class="absolute top-8 left-2 w-10 h-0.5 bg-white"></div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
